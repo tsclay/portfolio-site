d3c09ff28c296a22184acbbe1565f31f8408e92e
Tim <tsclay9@gmail.com>
2020-06-30 14:35:31 -0400

    messing with svelte

---
+++ src/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  h1 {
+    color: #00ccff;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
+
+<header>
+  <h1>Test {name}</h1>
+  <h1>This is pretty cool.</h1>
+</header>
