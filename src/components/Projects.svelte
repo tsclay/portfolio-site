a1f8fadc221d704f84a18082e98532b6882fd8b5
Tim <tsclay9@gmail.com>
2020-07-10 22:49:16 -0400

    added carousel; can go forward, back, play and stop

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,13 +1,56 @@
 <script>
-  let nums = [1, 2, 3, 4];
+  import { onMount } from "svelte";
+  let links = [
+    "/img/tim.jpg",
+    "/img/drawing.svg",
+    "/img/tim.jpg",
+    "/favicon.png"
+  ];
+  let duration = 750;
+  let timer = null;
+
+  const next = () => {
+    const lastLink = links.shift();
+    links = [...links, lastLink];
+  };
+
+  const prev = () => {
+    const nextLink = links.pop();
+    links = [nextLink, ...links];
+  };
+
+  // Start carousel reel
+  const play = () => {
+    timer = setInterval(next, duration);
+    console.log("timer started");
+  };
+
+  const stop = () => {
+    console.log("stop!");
+    return clearTimeout(timer);
+  };
+
+  play();
 </script>
 
 <style>
-
+  #project-carousel {
+    /* overflow: hidden; */
+    position: relative;
+  }
 </style>
 
 <div id="project-carousel">
-  {#each nums as num}
-    <div id="project-{num}" />
-  {/each}
+  <div class="flex-row nowrap">
+    {#each links as link}
+      <div id="project-{link}">
+        <img src={link} alt="" />
+      </div>
+    {/each}
+  </div>
+
+  <button on:click={next} type="button">Forward</button>
+  <button on:click={prev} type="button">Back</button>
+  <button on:click={stop} type="button">STOP</button>
+  <button on:click={play} type="button">Play</button>
 </div>
