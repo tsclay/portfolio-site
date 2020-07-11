cea80939f0f27837f4817cc08b1155032276e6fd
Tim <tsclay9@gmail.com>
2020-07-11 01:22:36 -0400

    added code from another repo for flipping cards; need to look for 'svelte way' of doing it

--- src/components/ProfileImg.svelte
+++ src/components/ProfileImg.svelte
@@ -7,8 +7,48 @@
     width: 100%;
     height: 100%;
   }
+
+  .card-inner:hover {
+    transform: rotateY(180deg);
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+  }
+
+  .card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
+  }
+
+  .card-front,
+  .card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+  }
+
+  .card-back {
+    background-color: #333;
+    color: white;
+    /* padding: 20px; */
+    transform: rotateY(180deg);
+  }
 </style>
 
 <div id="profile">
-  <img src="/img/tim.jpg" alt="Tim" />
+  <div class="card-inner">
+    <div class="card-front">
+      <img src="/img/tim.jpg" alt="Tim" />
+    </div>
+    <div class="card-back">
+      <img src="/img/drawing.svg" alt="name" />
+    </div>
+  </div>
+  <div />
 </div>
