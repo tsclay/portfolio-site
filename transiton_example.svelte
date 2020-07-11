e9c90b4a3bb2940961b23284ad693487c79f37b0
Tim <tsclay9@gmail.com>
2020-07-11 00:55:31 -0400

    proof of concept

---
+++ transiton_example.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { fade, fly } from "svelte/transition";
+  let visible = true;
+
+  setInterval(() => {
+    visible = !visible;
+  }, 5000);
+</script>
+
+<style>
+  .parent {
+    position: relative;
+  }
+
+  .child {
+    position: absolute;
+    top: 0;
+  }
+
+  .child-1 {
+    left: 0;
+  }
+  .child-2 {
+    left: 0;
+  }
+</style>
+
+<label>
+  <input type="checkbox" bind:checked={visible} />
+  visible
+</label>
+
+<div class="parent">
+  {#if visible}
+    <div
+      class="child child-1"
+      in:fly={{ x: 200, duration: 2100 }}
+      out:fly={{ x: -200, duration: 2000 }}>
+      Flies in, fades out
+    </div>
+  {:else}
+    <div
+      class="child child-2"
+      in:fly={{ x: 200, duration: 2100 }}
+      out:fly={{ x: -200, duration: 2000 }}>
+      What up
+    </div>
+  {/if}
+</div>
