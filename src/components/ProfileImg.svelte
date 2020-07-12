f3e8e057727cea0d639d65b6d2e3253101903437
Tim <tsclay9@gmail.com>
2020-07-12 13:33:01 -0400

    integrated flip card functionality

--- src/components/ProfileImg.svelte
+++ src/components/ProfileImg.svelte
@@ -1,36 +1,47 @@
 <script>
-  import { elasticOut } from "svelte/easing";
+  import {
+    sineOut,
+    sineIn,
+    linear,
+    cubicIn,
+    elasticIn,
+    sineInOut,
+    cubicInOut
+  } from "svelte/easing";
 
-  // const flip = (node, duration) => {
-  //   return {
-  //     duration,
-  //     css: t => {
-  //       const eased = elasticOut(t);
+  let step = 0;
 
-  //       return `
-  //       transform: rotateY(${eased * 180}deg);
-  //       transform-style: preserve-3d;
-  //       -webkit-backface-visibility: hidden;
-  //       backface-visibility: hidden;`;
-  //     }
-  //   };
-  // };
+  const toggleDiv = () => {
+    step = (step + 1) % 2;
+  };
+
+  // The div is in ON position when its rotateY deg is 0
+  // The div is in OFF position when rotateY is 180 or -180
+  // When div is OFF, its backside is exposed
+  // Since backside is hidden, nothing is seen if no other div follows
+  const flip = (node, { duration }) => {
+    return {
+      duration,
+      css: t => {
+        const eased = cubicInOut(t);
+        const test = (eased - 1) * 180;
+        // 				if (step > 0) console.log('green', test)
+        // 				if (step === 0) console.log('red', test)
+
+        return `transform: rotateY(${(eased - 1) * 180}deg);
+    transform-style: preserve-3d;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;`;
+      }
+    };
+  };
 </script>
 
 <style>
-  .card-inner:hover {
-    transform: rotateY(180deg);
-    transition: transform 0.8s;
-    transform-style: preserve-3d;
-    -webkit-backface-visibility: hidden;
-    backface-visibility: hidden;
-  }
-
   .card-inner {
     position: relative;
     width: 100%;
     height: 100%;
-    transition: transform 0.8s;
     transform-style: preserve-3d;
   }
 
@@ -46,19 +57,20 @@
   .card-back {
     background-color: #333;
     color: white;
-    /* padding: 20px; */
-    transform: rotateY(180deg);
   }
 </style>
 
 <div id="profile">
-  <div class="card-inner">
-    <div class="card-front">
-      <img class="img-fill" src="/img/tim.jpg" alt="Tim" />
-    </div>
-    <div class="card-back">
-      <img class="img-fill" src="/img/drawing.svg" alt="name" />
-    </div>
+  <div on:click={toggleDiv} class="card-inner">
+    {#if step === 0}
+      <div transition:flip={{ duration: 800 }} class="card-front">
+        <img class="img-fill" src="/img/tim.jpg" alt="Tim" />
+      </div>
+    {:else}
+      <div transition:flip={{ duration: 800 }} class="card-back">
+        <img class="img-fill" src="/img/drawing.svg" alt="name" />
+      </div>
+    {/if}
   </div>
   <div />
 </div>
