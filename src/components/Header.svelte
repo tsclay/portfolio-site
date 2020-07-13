54240246fa1761448f45bea71f64d63d98dd1a3d
Tim <tsclay9@gmail.com>
2020-07-13 12:30:20 -0400

    combined Brand into Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -6,18 +6,48 @@
   p {
     color: #1a333a;
     margin-top: 0;
-    margin-bottom: 0;
+    margin-bottom: 0.5em;
     text-transform: uppercase;
-    font-size: 2em;
     font-weight: 100;
   }
 
   #header {
     background: burlywood;
   }
+
+  #title {
+    font-size: 4em;
+  }
+  #subtitle {
+    left: 15%;
+    font-size: 2em;
+  }
 </style>
 
-<div id="header">
-  <p>This is the header.</p>
-  <!-- <img class="img-background" src="/img/tim.jpg" alt="My name is {name}" /> -->
+<div id="header" class="flex-row nowrap flex-justify-between flex-align-center">
+  <div class="w-50 h-100">
+    <div class="pos-relative">
+      <p class="pos-relative" id="title">Tim Clay</p>
+      <p class="pos-absolute" id="subtitle">Software Developer</p>
+    </div>
+  </div>
+
+  <div id="footer" class="w-50">
+    <ul
+      class="w-30 flex-column flex-justify-between flex-align-start ul-list-none"
+      style="margin: 0 auto;">
+      <li class="mb-1">
+        About
+        <i class="fas fa-info-circle" />
+      </li>
+      <li class="mb-1">
+        Contact
+        <i class="fas fa-envelope" />
+      </li>
+      <li class="mb-1">
+        GitHub
+        <i class="fab fa-github" />
+      </li>
+    </ul>
+  </div>
 </div>
