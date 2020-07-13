2d4eb92e1ab0712f86b492be1957798c9ef1c209
Tim Clay <tsclay9@gmail.com>
2020-07-12 21:58:06 -0400

    clean up code

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,77 +1,71 @@
 <script>
-  import { onMount } from "svelte";
-  import { fly } from "svelte/transition";
-  import { linear, cubicOut } from "svelte/easing";
+  import { onMount } from 'svelte'
+  import { fly, fade } from 'svelte/transition'
+  import { linear, cubicOut } from 'svelte/easing'
   import {
     SkipForwardIcon,
     SkipBackIcon,
     PlayIcon,
     PauseIcon
-  } from "svelte-feather-icons";
-  let links = ["/img/tim.jpg", "/img/drawing.svg", "/favicon.png"];
-  let duration = 5000;
-  let timer = null;
-  let isPlaying = false;
-  let step = 0;
-  const len = links.length;
-  let flowDir = 200;
-  let inverseFlowDir = flowDir * -1;
-  $: inverseFlowDir = flowDir * -1;
-
-  const forwardMoton = ``;
+  } from 'svelte-feather-icons'
+  let links = ['/img/tim.jpg', '/img/drawing.svg', '/favicon.png']
+  let duration = 5000
+  let timer = null
+  let isPlaying = false
+  let step = 0
+  const len = links.length
+  let hide = true
+  let flowDir = 200
+  let inverseFlowDir = flowDir * -1
+  $: inverseFlowDir = flowDir * -1
 
   onMount(() => {
-    play();
-  });
-
-  // const fixDivOnPage = () => {
-  //   const el = document.getElementById(`project-${step}`)
-
-  // }
+    play()
+  })
 
   const next = () => {
-    console.log("next");
-    if (isPlaying) stop();
-    flowDir = 200;
-    step = (step + 1) % len;
-    console.log("next:", step);
-  };
+    console.log('next')
+    if (isPlaying) stop()
+    flowDir = 200
+    step = (step + 1) % len
+    console.log('next:', step)
+  }
 
   const prev = () => {
-    console.log("prev");
-    if (isPlaying) stop();
-    flowDir = -200;
+    console.log('prev')
+    if (isPlaying) stop()
+    flowDir = -200
     // console.log("inversed", inverseFlowDir);
-    step = (step - 1 + len) % len;
-    console.log("prev:", step);
-  };
+    step = (step - 1 + len) % len
+    console.log('prev:', step)
+  }
 
   // Start carousel reel
   const play = () => {
-    if (isPlaying) return;
-    isPlaying = true;
-    flowDir = 200;
+    if (isPlaying) return
+    isPlaying = true
+    flowDir = 200
     timer = setInterval(() => {
-      step = (step + 1) % len;
-    }, duration);
-    console.log("playing!");
-  };
+      step = (step + 1) % len
+    }, duration)
+    console.log('playing!')
+  }
 
   const stop = () => {
-    isPlaying = false;
-    console.log("stop!");
-    return clearTimeout(timer);
-  };
+    isPlaying = false
+    console.log('stop!')
+    return clearTimeout(timer)
+  }
 
   const goOut = (
     node,
     { duration, opacity = 0, y = 0, easing: easing$1 = cubicOut }
   ) => {
-    const x = inverseFlowDir;
-    const style = getComputedStyle(node);
-    const target_opacity = +style.opacity;
-    const transform = style.transform === "none" ? "" : style.transform;
-    const od = target_opacity * (1 - opacity);
+    const x = inverseFlowDir
+    const style = getComputedStyle(node)
+    const target_opacity = +style.opacity
+    const transform = style.transform === 'none' ? '' : style.transform
+    const od = target_opacity * (1 - opacity)
     return {
       duration,
       easing: easing$1,
@@ -79,20 +73,20 @@
       css: (t, u) => {
         return `transform: ${transform} translate(${(1 - t) * x}px, ${(1 - t) *
           y}px);
-        opacity: ${target_opacity - od * u}`;
+        opacity: ${target_opacity - od * u}`
       }
-    };
-  };
+    }
+  }
 
   const goIn = (
     node,
     { duration, opacity = 0, y = 0, easing: easing$1 = cubicOut }
   ) => {
-    const x = flowDir;
-    const style = getComputedStyle(node);
-    const target_opacity = +style.opacity;
-    const transform = style.transform === "none" ? "" : style.transform;
-    const od = target_opacity * (1 - opacity);
+    const x = flowDir
+    const style = getComputedStyle(node)
+    const target_opacity = +style.opacity
+    const transform = style.transform === 'none' ? '' : style.transform
+    const od = target_opacity * (1 - opacity)
     return {
       duration,
       easing: easing$1,
@@ -100,10 +94,18 @@
       css: (t, u) => {
         return `transform: ${transform} translate(${(1 - t) * x}px, ${(1 - t) *
           y}px);
-        opacity: ${target_opacity - od * u}`;
+        opacity: ${target_opacity - od * u}`
       }
-    };
-  };
+    }
+  }
+
+  const showControls = () => {
+    hide = false
+  }
+
+  const hideControls = () => {
+    hide = true
+  }
 
   //   function fly(node, { delay = 0, duration = 400, easing: easing$1 = easing.cubicOut, x = 0, y = 0, opacity = 0 }) {
   //     const style = getComputedStyle(node);
@@ -134,7 +136,10 @@
   }
 </style>
 
-<div id="project-carousel">
+<div
+  id="project-carousel"
+  on:mouseenter={showControls}
+  on:mouseleave={hideControls}>
   <div class="pos-relative h-100">
     {#each links as link, i}
       {#if step === i}
@@ -147,21 +152,24 @@
         </div>
       {/if}
     {/each}
-    <div
-      id="controls"
-      class="pos-absolute w-100 flex-row flex-align-center flex-justify-center">
-      <button on:click={prev} type="button">
-        <SkipBackIcon size="1.0x" />
-      </button>
-      <button on:click={next} type="button">
-        <SkipForwardIcon size="1.0x" />
-      </button>
-      <button on:click={stop} type="button">
-        <PauseIcon size="1.0x" />
-      </button>
-      <button on:click={play} type="button">
-        <PlayIcon size="1.0x" />
-      </button>
-    </div>
+    {#if !hide}
+      <div
+        id="controls"
+        transition:fade={{ duration: 300 }}
+        class="pos-absolute w-100 flex-row flex-align-center flex-justify-center">
+        <button on:click={prev} type="button">
+          <SkipBackIcon size="1.0x" />
+        </button>
+        <button on:click={next} type="button">
+          <SkipForwardIcon size="1.0x" />
+        </button>
+        <button on:click={stop} type="button">
+          <PauseIcon size="1.0x" />
+        </button>
+        <button on:click={play} type="button">
+          <PlayIcon size="1.0x" />
+        </button>
+      </div>
+    {/if}
   </div>
 </div>
