434d49e949fbd714368aa1dd4a94c9cee78631a5
Tim <tsclay9@gmail.com>
2020-07-24 22:55:01 -0400

    added snipsnap project

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -15,25 +15,35 @@
       image: "/img/berg.png",
       url: "http://berg-musicapp.herokuapp.com/login",
       description:
-        "Berg is an app that allows musicians interested in atonal set theory to easily see the transformations of the sets they input. This app utilizes PHP Laravel to serve the views, which are scaffolded by React.js UI components, and to act as the backend connecting to a PostgreSQL database."
+        "Berg is an app that allows musicians interested in atonal set theory to easily see the transformations of the sets they input. This app utilizes PHP Laravel to serve the views, which are scaffolded by React.js UI components, and to act as the backend connecting to a PostgreSQL database.",
+      tags: ["PostgreSQL", "Javascript", "PHP", "Laravel", "React"]
     },
     {
       title: "Songster",
       image: "/img/songster.png",
       url: "https://tsclay.github.io/songster.github.io/",
       description:
-        "Songster is an SPA that utilizes the jQuery library to make AJAX requests to the Genius Music API. Once a user selects a song, jQuery scrapes its lyrics from the HTML and formats it for the user's reading/singing pleasure."
+        "Songster is an SPA that utilizes the jQuery library to make AJAX requests to the Genius Music API. Once a user selects a song, jQuery scrapes its lyrics from the HTML and formats it for the user's reading/singing pleasure.",
+      tags: ["Javascript", "jQuery"]
     },
     {
       title: "Lisa Hospitality Solutions",
       image: "/img/lisa.png",
       url: "https://lisahospitalitysolutions.herokuapp.com/",
       description:
-        "Lisa is a full CRUD app that implements user authentication and authorization. Users create forum entries relating to a guest's experience at their resort. Users can comment and can update or delete their comments. The app is server-rendered using Express.js, the Node.js runtime, and the EJS view engine."
+        "Lisa is a full CRUD app that implements user authentication and authorization. Users create forum entries relating to a guest's experience at their resort. Users can comment and can update or delete their comments. The app is server-rendered using Express.js, the Node.js runtime, and the EJS view engine.",
+      tags: ["Javascript", "EJS", "Node", "Express", "MongoDB"]
+    },
+    {
+      title: "SnipSnap",
+      image: "/img/snipsnap.png",
+      url: "https://snipsnapapp.herokuapp.com/",
+      description:
+        "SnipSnap was bourne of a pair-programming collaboration. It is a full CRUD app for code snippets. As of now, it supports CSS, JS, and HTML. It uses codePrettify and Prettier to format and highlight the code snippets called from the database. Users can also search by snippet name to filter results.",
+      tags: ["Javascript", "React", "PHP", "PostgreSQL"]
     }
   ];
 
-  // https://snipsnapapp.herokuapp.com/
   let duration = 8000;
   let timer = null;
   let isPlaying = false;
@@ -183,6 +193,18 @@
     height: 100%;
     width: 100%;
   }
+
+  .btn-blue {
+    display: block;
+    background: #00ccff;
+    color: rgb(48, 48, 48);
+    border-radius: 4px;
+    margin: 0 auto;
+  }
+
+  .btn-blue:hover {
+    color: rgb(196, 196, 196);
+  }
 </style>
 
 <!-- style="height: 400px;" -->
@@ -206,7 +228,7 @@
             <h1>{link.title}</h1>
             <p>{link.description}</p>
             <form action={link.url} target="_blank">
-              <button type="submit">Check it out!</button>
+              <button class="btn-blue" type="submit">Check it out!</button>
             </form>
           </div>
         </div>
