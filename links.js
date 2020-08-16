30938c0192f91cb3536e0c30aa6775c57daacebc
Tim <tsclay9@gmail.com>
2020-08-16 15:01:23 -0400

    added links as object in separate file

---
+++ links.js
@@ -0,0 +1,34 @@
+let links = [
+    {
+      title: "Berg",
+      image: "/img/berg.png",
+      url: "http://berg-musicapp.herokuapp.com/login",
+      description:
+        "Berg is an app that allows musicians interested in atonal set theory to easily see the transformations of the sets they input. This app utilizes PHP Laravel to serve the views, which are scaffolded by React.js UI components, and to act as the backend connecting to a PostgreSQL database.",
+      tags: ["PostgreSQL", "Javascript", "PHP", "Laravel", "React"]
+    },
+    {
+      title: "Songster",
+      image: "/img/songster.png",
+      url: "https://tsclay.github.io/songster.github.io/",
+      description:
+        "Songster is an SPA that utilizes the jQuery library to make AJAX requests to the Genius Music API. Once a user selects a song, jQuery scrapes its lyrics from the HTML and formats it for the user's reading/singing pleasure.",
+      tags: ["Javascript", "jQuery"]
+    },
+    {
+      title: "Lisa Hospitality Solutions",
+      image: "/img/lisa.png",
+      url: "https://lisahospitalitysolutions.herokuapp.com/",
+      description:
+        "Lisa is a full CRUD app that implements user authentication and authorization. Users create forum entries relating to a guest's experience at their resort. Users can comment and can update or delete their comments. The app is server-rendered using Express.js, the Node.js runtime, and the EJS view engine.",
+      tags: ["Javascript", "EJS", "Node", "Express", "MongoDB"]
+    },
+    {
+      title: "SnipSnap",
+      image: "/img/snipsnap.png",
+      url: "https://snipsnapapp.herokuapp.com/",
+      description:
+        "SnipSnap was bourne of a pair-programming collaboration. It is a full CRUD app for code snippets. As of now, it supports CSS, JS, and HTML. It uses codePrettify and Prettier to format and highlight the code snippets called from the database. Users can also search by snippet name to filter results.",
+      tags: ["Javascript", "React", "PHP", "PostgreSQL"]
+    }
+  ];
